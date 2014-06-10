1532b5ae543d8e728a8c0fa6930432fa1c9b0305
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-06-10 17:11:08 +0200

    Inital commit

---
+++ run.sh
@@ -0,0 +1,13 @@
+if [ ! -n "$NPM_EMAIL" ]; then
+  error 'Please specify email'
+  exit 1
+fi
+
+if [ ! -n "$NPM_AUTH_TOKEN" ]; then
+  error 'Please specify auth token'
+  exit 1
+fi
+
+echo _auth = $NPM_AUTH_TOKEN > ~/.npmrc_new
+echo email = $NPM_EMAIL >> ~/.npmrc_new
+npm publish
